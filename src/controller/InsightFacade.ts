b09c175722b7d43c9d19eb1901691e9dd5d3184a
k1c2b <k1c2b@ugrad.cs.ubc.ca>
2020-01-23 20:32:22 -0800

    fixed addDataset so that it waits until all files have been read and paresed before resloving. I also cleaned up some variable names

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -16,86 +16,94 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-        let foo: JSZip = new JSZip();
+        let zip: JSZip = new JSZip();
         let courses: Course[] = [];
         let p1 = new Promise<string[]>((resolve, reject) => {
-            foo.loadAsync(content, { base64: true}).then(function (result: JSZip) {
-                foo.forEach(function (relativePath, file) {
+            zip.loadAsync(content, { base64: true}).then(function (files: JSZip) {
+                let promises: Array<Promise<any>> = [];
+                files.forEach( (relativePath, file) => {
                     // eslint-disable-next-line no-console
                     // console.log("iterating over", relativePath);
                     // let courses: Course[] = [];
-                    foo.file(relativePath).async("text").then(function success( fum) {
+                    // I had to add this because for some reason the first iteration of the courses test was NULL
+                    let openedFile: any = files.file(relativePath);
+                    if (openedFile) {
+                        promises.push(openedFile.async("text"));
+                    }
+                });
+                // Once all of the files have finished being read continue
+                Promise.all(promises).then(function success( allFiles) {
+                    allFiles.forEach(function (file) {
                         // Parse the file into a JSON object
-                        let json: any = JSON.parse(fum);
-                        // Get the array of results from the file
-                        let jjson: any = json["result"];
+                        let json: any = JSON.parse(file);
+                        // Get the array of files from the file
+                        let sections: any = json["result"];
                         // Go through all of the sections in the file
-                        for (let i in jjson) {
+                        for (let section of sections) {
                             let curCourse: Course = new Course();
                             // Go through all of the elements of a section
-                            for (let j in jjson[i]) {
-                                InsightFacade.findValidFields(j, curCourse, jjson, i);
+                            for (let fieldName in section) {
+                                InsightFacade.findValidFields(fieldName, curCourse, section);
                             }
                             // TODO if the course is missing any fields don't add it
                             courses.push(curCourse);
                         }
-                        for (let i in courses) {
-                            // eslint-disable-next-line no-console
-                            console.log(courses[i]);
-                        }
-                        let ids: string[] = [id];
-                        // datasets.push(new Dataset(id, courses));
-                        // for (let i in datasets) {
-                        //     ids.push(datasets[i].id);
-                        // }
-                        resolve(ids);
-
-                    }, function error(e) {
-                        // TODO better reject message
-                        reject("why?");
-                        // handle the error
                     });
-
+                    for (let i in courses) {
+                        // eslint-disable-next-line no-console
+                        console.log(courses[i]);
+                    }
+                    // TODO build a proper dataset and then return all of the ids in the datasets lists
+                    let ids: string[] = [id];
+                    // datasets.push(new Dataset(id, courses));
+                    // for (let i in datasets) {
+                    //     ids.push(datasets[i].id);
+                    // }
+                    resolve(ids);
+                }, function error(e) {
+                    // TODO better reject message
+                    reject("why?");
+                    // handle the error
                 });
-                // eslint-disable-next-line no-console
-                console.log("here");
+            }).catch((error: any) => {
+                reject(error);
             });
         });
         return p1;
         // return Promise.reject("Not implemented.");
     }
 
-    private static findValidFields(j: string, curCourse: Course, jjson: any, i: string) {
-        switch (j) {
+    private static findValidFields(fieldName: string, curCourse: Course, section: any) {
+        switch (fieldName) {
             case "Subject":
-                curCourse.dept = jjson[i][j];
+                curCourse.dept = section[fieldName];
                 break;
             case "Course":
-                curCourse.id = jjson[i][j];
+                curCourse.id = section[fieldName];
                 break;
             case "Avg":
-                curCourse.avg = jjson[i][j];
+                curCourse.avg = section[fieldName];
                 break;
             case "Professor":
-                curCourse.instructor = jjson[i][j];
+                curCourse.instructor = section[fieldName];
                 break;
             case "Title":
-                curCourse.title = jjson[i][j];
+                curCourse.title = section[fieldName];
                 break;
             case "Pass":
-                curCourse.pass = jjson[i][j];
+                curCourse.pass = section[fieldName];
                 break;
             case "Fail":
-                curCourse.fail = jjson[i][j];
+                curCourse.fail = section[fieldName];
                 break;
             case "Audit":
-                curCourse.audit = jjson[i][j];
+                curCourse.audit = section[fieldName];
                 break;
             case "id":
-                curCourse.uuid = jjson[i][j];
+                curCourse.uuid = section[fieldName];
                 break;
             case "Year":
-                curCourse.year = jjson[i][j];
+                curCourse.year = section[fieldName];
                 break;
         }
     }
