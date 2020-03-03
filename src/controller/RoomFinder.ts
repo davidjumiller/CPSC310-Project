2f029dbae5679e183533d5f1f02eb53c6bc9cc42
neumann1 <neumann1@students.cs.ubc.ca>
2020-03-03 15:09:17 -0800

    implemented getLatLon but everything now needs to be promise based

--- src/controller/RoomFinder.ts
+++ src/controller/RoomFinder.ts
@@ -24,6 +24,7 @@ export class RoomFinder {
         }
         // tbody is always the 4th (3) obj in the childNodes array
         let tbody: any = table.childNodes[3];
+        let promises: Array<Promise<any>> = [];
         if (tbody.nodeName === "tbody") {
             tbody.childNodes.forEach((tr: any) => {
                 if (tr.childNodes !== undefined) {
@@ -44,8 +45,7 @@ export class RoomFinder {
                     building.shortname = td2.childNodes[0].value.trim();
                     building.fullname = td3.childNodes[1].childNodes[0].value.trim();
                     building.address = td4.childNodes[0].value.trim();
-                    RoomFinder.findLatLon(building);
-
+                    promises.push(RoomFinder.findLatLon(building));
                     buildings.push(building);
                 }
             });
@@ -139,17 +139,31 @@ export class RoomFinder {
         return roomsInBuilding;
     }
 
-    // TODO
-    private static findLatLon(building: Building) {
+    private static findLatLon(building: Building): Promise<any> {
         let url: string = "http://cs310.students.cs.ubc.ca:11316/api/v1/project_team131/";
         let URLEncodedAddress: string = building.address.replace(/\s/g, "%20");
         url = url.concat(URLEncodedAddress);
-        // try {
-        //     http.get(url, (res) => {
-        //         Log.trace(res);
-        //     });
-        // } catch (e) {
-        //     Log.trace("fluff");
-        // }
+        let p1: Promise<any> = new Promise<any>((resolve, reject) => {
+            try {
+                http.get(url, (res) => {
+                    let data: string = "";
+                    res.on("data", (chunk) => {
+                        data += chunk;
+                    });
+                    res.on("end", () => {
+                        let responsJson: any = JSON.parse(data);
+                        if (responsJson.lat) {
+                            building.lat = responsJson.lat;
+                            building.lon = responsJson.lon;
+                        } else {
+                            throw (new InsightError(responsJson.error));
+                        }
+                    });
+                });
+            } catch (e) {
+                throw (new InsightError("Invalid URL"));
+            }
+        });
+        return p1;
     }
 }
